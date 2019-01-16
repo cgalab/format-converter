b233c534029d3597699fc6ba277d6870e9965814
Peter Palfrader <peter@palfrader.org>
2019-01-16 08:31:24 +0100

    Implement .graphml passthrough "converter"

---
+++ ORD53/formats/GraphML.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""Passthrough for .graphml formatted graphs."""
+
+if __name__ == '__main__' and __package__ is None:
+    import os
+    __LEVEL = 2
+    os.sys.path.append(os.path.abspath(os.path.join(*([os.path.dirname(__file__)] + ['..']*__LEVEL))))
+
+from ORD53.graph.Graph import GeometricGraph
+from ORD53.common.geometry import Vertex2
+from ORD53.common.iter import pair_iterator, PeekIterator
+
+import os
+
+class GraphMLPassthroughException(Exception):
+    pass
+
+class GraphMLPassthroughGraph:
+    def __init__(self, f):
+        self.graphml = f.read()
+        if not self.graphml.startswith('<graphml'):
+            raise GraphMLPassthroughException("Not a graphml file")
+
+    def randomize_weights(self, rnd_lower=0.0, rnd_upper=5.0):
+        raise GraphMLPassthroughException("randomize_weights() not implemented for passthrough.")
+
+    def write_graphml(self, f):
+        """Write a graphml representation to the file f"""
+        f.write(self.graphml.encode('UTF-8'))
+
+class GraphMLLoader:
+    extension = '.graphml'
+
+    @classmethod
+    def load(cls, f, args=None):
+        g = GraphMLPassthroughGraph(f)
+        return g
