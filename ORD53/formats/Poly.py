bd730963a758e6d2bed942204e26a3d84e740e15
Günther Eder <geder@cosy.sbg.ac.at>
2019-09-11 11:27:25 +0200

    poly format added

--- ORD53/formats/Poly.py
+++ ORD53/formats/Poly.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""Loader for .line formatted graphs."""
+"""Loader for .poly formatted graphs."""
 
 if __name__ == '__main__' and __package__ is None:
     import os
@@ -11,27 +11,12 @@ from ORD53.graph.Graph import GeometricGraph
 from ORD53.common.geometry import Vertex2
 from ORD53.common.iter import pair_iterator, PeekIterator
 
+from ORD53.formats.Line import LineLoader
+
 import os
 
-class LineLoader:
-    extension = '.line'
-
-    """Load a graph from Martin's .line format"""
-    @staticmethod
-    def _add_polychain(g, f, close = False):
-        """Add a single polychain from the file"""
-        vertices = []
-        num_elems = int(next(f))
-        for _ in range(num_elems):
-            (x, y) = [float(c) for c in next(f).split()]
-            vertices.append(Vertex2(x, y))
-
-        for t in pair_iterator(vertices):
-            g.add_edge_by_vertex(*t)
-
-        if close:
-            g.add_edge_by_vertex( vertices[-1],vertices[0] )
-
+class PolyLoader:
+    extension = '.poly'
 
     @classmethod
     def load(cls, content, name="unknown", args=None):
@@ -45,7 +30,8 @@ class LineLoader:
                     continue
             except StopIteration:
                 break
-            cls._add_polychain(g, f)
+            LineLoader._add_polychain(g, f, True)
+            
         return g
 
 def main():
@@ -54,13 +40,13 @@ def main():
     import sys
 
     parser = argparse.ArgumentParser(description='Load a graph from a .line file')
-    parser.add_argument('inputfile', help='Inputfile (.line)', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
+    parser.add_argument('inputfile', help='Inputfile (.poly)', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
     parser.add_argument('outputfile', help='Outputfile (.graphml)', nargs='?', type=argparse.FileType('wb'), default=sys.stdout.buffer)
     parser.add_argument('-r', '--randomize-weights', action='store_true', default=False, help='randomize edge weights')
 
     args = parser.parse_args()
 
-    g = LineLoader.load(args.inputfile.read())
+    g = PolyLoader.load(args.inputfile.read())
     if args.randomize_weights:
       g.randomize_weights()
     g.write_graphml(args.outputfile)
